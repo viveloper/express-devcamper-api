0a5ae61aa43be894f3ba78d0f1aef44345c4b64c
viveloper <usik1234@gmail.com>
2020-03-12 16:25:05 +0900

    MongoDB with mongoose

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
+// Body parser
+app.use(express.json());
 
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Display bootcamp ${req.params.id}` });
-});
+// Dev logging middleware
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
 
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });
